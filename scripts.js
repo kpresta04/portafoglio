ee8dde4072ca26ff51d4f8f1750e1be2900060af
Kellen Presta <kpresta04@gmail.com>
2020-02-01 21:49:15 -0600

    gifs added

--- scripts.js
+++ scripts.js
@@ -1,8 +1,23 @@
 $(document).ready(function() {
+  //Beer Me mouseover
+  $("#beerMeImg").on("mouseover", function() {
+    $(this).attr("src", "./assets/beerme.gif");
+  });
+  $("#beerMeImg").on("mouseout", function() {
+    $(this).attr("src", "./assets/beerme400.png");
+  });
+  //JS Code Quiz Mouseover
   $("#jsImg").on("mouseover", function() {
-    $(this).attr("src", "./assets/astronaut.png");
+    $(this).attr("src", "./assets/codeqiz2.gif");
   });
   $("#jsImg").on("mouseout", function() {
     $(this).attr("src", "./assets/codingquiz500.png");
+  });
+  //Dash mouseover
+  $("#dashImg").on("mouseover", function() {
+    $(this).attr("src", "./assets/dash.gif");
+  });
+  $("#dashImg").on("mouseout", function() {
+    $(this).attr("src", "./assets/weatherdash400.png");
   });
 });
