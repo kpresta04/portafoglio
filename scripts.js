ef8e3f4bdc7564d06f90dc84a7f804454abfe56c
Kellen Presta <kpresta04@gmail.com>
2020-02-17 10:33:12 -0600

    removed jquery

--- scripts.js
+++ scripts.js
@@ -1,31 +1,35 @@
-$(document).ready(function() {
+window.onload = () => {
+  const $ = function(selector) {
+    return document.querySelector(selector);
+  };
+
   //Beer Me mouseover
-  $("#beerMeImg").on("mouseover", function() {
-    $(this).attr("src", "./assets/beerme2.gif");
-  });
-  $("#beerMeImg").on("mouseout", function() {
-    $(this).attr("src", "./assets/beerme400.png");
-  });
-  //JS Code Quiz Mouseover
-  $("#jsImg").on("mouseover", function() {
-    $(this).attr("src", "./assets/codeqiz2.gif");
-  });
-  $("#jsImg").on("mouseout", function() {
-    $(this).attr("src", "./assets/codingquiz500.png");
-  });
-  //Dash mouseover
-  $("#dashImg").on("mouseover", function() {
-    $(this).attr("src", "./assets/dash2.gif");
-  });
-  $("#dashImg").on("mouseout", function() {
-    $(this).attr("src", "./assets/weatherdash400.png");
-  });
+  // $("#beerMeImg").on("mouseover", function() {
+  //   $(this).attr("src", "./assets/beerme2.gif");
+  // });
+  // $("#beerMeImg").on("mouseout", function() {
+  //   $(this).attr("src", "./assets/beerme400.png");
+  // });
+  // //JS Code Quiz Mouseover
+  // $("#jsImg").on("mouseover", function() {
+  //   $(this).attr("src", "./assets/codeqiz2.gif");
+  // });
+  // $("#jsImg").on("mouseout", function() {
+  //   $(this).attr("src", "./assets/codingquiz500.png");
+  // });
+  // //Dash mouseover
+  // $("#dashImg").on("mouseover", function() {
+  //   $(this).attr("src", "./assets/dash2.gif");
+  // });
+  // $("#dashImg").on("mouseout", function() {
+  //   $(this).attr("src", "./assets/weatherdash400.png");
+  // });
 
   //send button
-  $("#sendBtn").on("mouseover", function() {
-    $("#planeImg").attr("src", "./assets/planeHover.jpg");
+  $("#sendBtn").addEventListener("mouseover", function() {
+    $("#planeImg").src = "./assets/planeHover.jpg";
   });
-  $("#sendBtn").on("mouseout", function() {
-    $("#planeImg").attr("src", "./assets/plane.jpg");
+  $("#sendBtn").addEventListener("mouseout", function() {
+    $("#planeImg").src = "./assets/plane.jpg";
   });
-});
+};
