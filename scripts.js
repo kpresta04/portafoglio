f475e013f9ce7773f7481c1b16f465b090ccdb74
Kellen Presta <kpresta04@gmail.com>
2020-02-19 22:20:04 -0600

    footer hider workgin

--- scripts.js
+++ scripts.js
@@ -1,15 +1,30 @@
 window.onload = () => {
-  // function scrollFunc(e) {
-  // console.log(e.target.scrollTop);
   // console.log(document.body.scrollHeight);
-  // if (window.innerHeight + window.scrollY > document.body.clientHeight) {
-  //   $(".navFooter").hide();
-  // }
-  //   if (e.target.scrollTop > 4000) {
-  //     $(".navFooter").hide();
-  //   } else {
-  //     $(".navFooter").show();
-  //   }
-  // }
-  // document.body.addEventListener("scroll", scrollFunc);
+
+  function scrollFunc(e) {
+    // console.log(e.target.scrollTop);
+    // if (window.innerHeight + window.scrollY > document.body.clientHeight) {
+    //   $(".navFooter").hide();
+    // }
+    if (screen.width < 1200) {
+      if (
+        e.target.scrollTop >
+        document.body.scrollHeight - document.body.clientHeight - 100
+      ) {
+        $(".navFooter").hide();
+      } else {
+        $(".navFooter").show();
+      }
+    }
+  }
+
+  document.body.addEventListener("scroll", scrollFunc);
+  function getDocHeight() {
+    const D = document;
+    return Math.max(
+      D.body.scrollHeight,
+      D.body.offsetHeight,
+      D.body.clientHeight
+    );
+  }
 };
