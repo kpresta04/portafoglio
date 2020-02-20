6c23cc2ce2ebe16bca2c44bec87539fb3a4a346c
Kellen Presta <kpresta04@gmail.com>
2020-02-19 21:32:14 -0600

    added footer

--- scripts.js
+++ scripts.js
@@ -1,17 +1,15 @@
 window.onload = () => {
-  // function scrollFunc() {
-  //   if (
-  //     document.body.scrollTop > 1350 ||
-  //     document.documentElement.scrollTop > 1350
-  //   ) {
-  //     $(".navFooter").show();
-  //   } else {
+  // function scrollFunc(e) {
+  // console.log(e.target.scrollTop);
+  // console.log(document.body.scrollHeight);
+  // if (window.innerHeight + window.scrollY > document.body.clientHeight) {
+  //   $(".navFooter").hide();
+  // }
+  //   if (e.target.scrollTop > 4000) {
   //     $(".navFooter").hide();
+  //   } else {
+  //     $(".navFooter").show();
   //   }
   // }
   // document.body.addEventListener("scroll", scrollFunc);
-  // function showFooter() {
-  //   $(".navFooter").show();
-  // }
-  // setTimeout(showFooter, 3000);
 };
