8d582d8406e7b7f7712ececde06c77675b4f5aa6
Kellen Presta <kpresta04@gmail.com>
2020-02-19 19:35:04 -0600

    images added to footer

--- scripts.js
+++ scripts.js
@@ -1,61 +1,17 @@
 window.onload = () => {
-  // const $ = function(selector) {
-  //   return document.querySelector(selector);
-  // };
-  //Beer Me mouseover
-  // $("#beerMeImg").on("mouseover", function() {
-  //   $(this).attr("src", "./assets/beerme2.gif");
-  // });
-  // $("#beerMeImg").on("mouseout", function() {
-  //   $(this).attr("src", "./assets/beerme400.png");
-  // });
-  // //JS Code Quiz Mouseover
-  // $("#jsImg").on("mouseover", function() {
-  //   $(this).attr("src", "./assets/codeqiz2.gif");
-  // });
-  // $("#jsImg").on("mouseout", function() {
-  //   $(this).attr("src", "./assets/codingquiz500.png");
-  // });
-  // //Dash mouseover
-  // $("#dashImg").on("mouseover", function() {
-  //   $(this).attr("src", "./assets/dash2.gif");
-  // });
-  // $("#dashImg").on("mouseout", function() {
-  //   $(this).attr("src", "./assets/weatherdash400.png");
-  // });
-  // let screenWidth = window.matchMedia("(min-width: 1000px)");
-  // function changeWelcomeImage(screenWidth) {
-  //   if (screenWidth.matches) {
-  //     // If media query matches
-  //     $("#bannerPhoto").src = "./assets/comp.jpg";
+  // function scrollFunc() {
+  //   if (
+  //     document.body.scrollTop > 1350 ||
+  //     document.documentElement.scrollTop > 1350
+  //   ) {
+  //     $(".navFooter").show();
   //   } else {
-  //     $("#bannerPhoto").src = "./assets/black-laptop.jpg";
+  //     $(".navFooter").hide();
   //   }
   // }
-  // changeWelcomeImage(screenWidth);
-  // screenWidth.addListener(changeWelcomeImage);
-  //send button
-  // $("#sendBtn").addEventListener("mouseover", function() {
-  //   $("#planeImg").src = "./assets/planeHover.jpg";
-  // });
-  // $("#sendBtn").addEventListener("mouseout", function() {
-  //   $("#planeImg").src = "./assets/plane.jpg";
-  // });
-  function scrollFunc() {
-    if (
-      document.body.scrollTop > 1350 ||
-      document.documentElement.scrollTop > 1350
-    ) {
-      $(".navFooter").show();
-    } else {
-      $(".navFooter").hide();
-    }
-  }
-  document.body.addEventListener("scroll", scrollFunc);
-
+  // document.body.addEventListener("scroll", scrollFunc);
   // function showFooter() {
   //   $(".navFooter").show();
   // }
-
   // setTimeout(showFooter, 3000);
 };
